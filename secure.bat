27f996c1da0343d627ace6fd003ab486f380d06f
Akash <akash.w.shah@gmail.com>
2021-12-10 23:31:16 -0500

    Delete disallowed media files functionality

--- secure.bat
+++ secure.bat
@@ -258,4 +258,52 @@ if %ERRORLEVEL% equ 1 (
     echo ------------------------------------------------------------------------------------
     echo:
 )
+
+Rem Disallowed Media Files
+choice /c ync /m "Do you wish to remove disallowed media files? (Manual and automatic mode are available) "
+if %ERRORLEVEL% equ 3 goto:eof
+if %ERRORLEVEL% equ 2 echo Skipping disallowed media files...
+if %ERRORLEVEL% equ 1 (
+    choice /c amc /m "Manual or automatic mode? (Manual mode steps through each file type and file while automatic mode disables them all) (MANUAL MODE IS HIGHLY RECOMMENDED AS YOU WILL NOT ACCIDENTALLY DELETE IMAGES/MEDIA USED BY APPLICATIONS) "
+    if !ERRORLEVEL! equ 3 (
+        echo Skipping disallowed media files... 
+    ) else (
+        set filetypes=mp3 mov mp4 avi mpg mpeg flac m4a flv ogg gif png jpg jpeg
+        cd C:\Users
+        echo ------------------------------------------------------------------------------------
+        echo *** Deleting disallowed media file types...                                      ***
+        Rem Automatic mode
+        if !ERRORLEVEL! equ 1 (
+            for %%i in (!filetypes!) do (
+                echo Deleting all .%%i files...
+                for /f "delims=" %%a in ('dir /s /b *.%%i') do (
+                    echo Deleting %%a...
+                    del %%a
+                )
+            )
+        Rem Manual mode
+        ) else (
+            for %%i in (!filetypes!) do (
+                choice /c yn /m "Do you wish to delete .%%i files? "
+                if !ERRORLEVEL! equ 1 (
+                    echo Deleting .%%i files...
+                    for /f "delims=" %%a in ('dir /s /b *.%%i') do (
+                        choice /c yn /m "Do you wish to delete %%a? "
+                        if !ERRORLEVEL! equ 1 (
+                            echo Deleting %%a...
+                            del %%a
+                        ) else (
+                            echo Skipping %%a...
+                        )
+                    )
+                ) else (
+                    echo Skipping .%%i files...
+                )
+            )
+        )
+        echo *** Finished                                                                     ***
+        echo ------------------------------------------------------------------------------------
+    )
+)
+
 pause
